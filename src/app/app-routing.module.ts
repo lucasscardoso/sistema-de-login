9e389b061eeaa7c66e4f7cba362d84240271345d
lucasscardoso <gajeel.red.fox@hotmai.com>
2024-09-23 10:43:12 -0300

    criados modulos, components e configurações de scss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminModule } from './components/admin/admin.module';
+import { AuthModule } from './core/components/auth/auth.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./core/components/auth/auth.module').then(m => AuthModule)
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./components/admin/admin.module').then(m => AdminModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
