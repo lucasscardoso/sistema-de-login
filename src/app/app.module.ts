e350baccde105f2c75b1f8e749cce39b343fb9a3
lucasscardoso <gajeel.red.fox@hotmai.com>
2024-09-23 14:25:01 -0300

    alterados os imports entre os modulos, foi implementada a logica inicial no sign ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -14,7 +15,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
